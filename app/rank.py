fde6960cc4303adf7ebb0214f5dc61b9ab440cb9
christopherfkk <christopherfkk@gmail.com>
2023-02-25 13:59:05 +0530

    Add error handling for add score

--- app/rank.py
+++ app/rank.py
@@ -39,56 +39,70 @@ def update():
         self_score = request.form['your-score']
         opp_score = request.form['opponent-score']
 
-        # Query their id, current skill, current uncertainty
         db = get_db()
+        error = None
+        usernames = [row["username"] for row in db.execute('SELECT username FROM d_user').fetchall()]
+        if not opp_username or not self_score or not opp_score:
+            error = 'All entries must be filled.'
+        elif opp_username not in usernames:
+            error = 'Opponent\'s username does not exist.'
+        elif not self_score.isdigit() or not opp_score.isdigit():
+            error = "Your/Opponent score must be an integer."
+        elif self_score == opp_score:
+            error = "Game result cannot be a draw."
 
-        self_ = db.execute(
-            """
-            SELECT du.id as user_id, ds.skill, ds.uncertainty
-            FROM d_user as du
-            JOIN d_skill ds on du.id = ds.id
-            WHERE du.id = ?
-            """,
-            (g.user["id"],)
-        ).fetchone()
+        # Query their id, current skill, current uncertainty
+        if error is None:
+            self_ = db.execute(
+                """
+                SELECT du.id as user_id, ds.skill, ds.uncertainty
+                FROM d_user as du
+                JOIN d_skill ds on du.id = ds.id
+                WHERE du.id = ?
+                """,
+                (g.user["id"],)
+            ).fetchone()
 
-        opp_ = db.execute(
-            """
-            SELECT du.id as user_id, ds.skill as skill, ds.uncertainty as uncertainty
-            FROM d_user as du
-            JOIN d_skill ds on du.id = ds.id
-            WHERE du.username = ?
-            """,
-            (opp_username,)
-        ).fetchone()
+            opp_ = db.execute(
+                """
+                SELECT du.id as user_id, ds.skill as skill, ds.uncertainty as uncertainty
+                FROM d_user as du
+                JOIN d_skill ds on du.id = ds.id
+                WHERE du.username = ?
+                """,
+                (opp_username,)
+            ).fetchone()
 
-        # Update their Rating (skill and uncertainty)
-        self_id, self_mu, self_sigma = self_["user_id"], self_["skill"], self_["uncertainty"]
-        opp_id, opp_mu, opp_sigma = opp_["user_id"], opp_["skill"], opp_["uncertainty"]
+            # Update their Rating (skill and uncertainty)
+            self_id, self_mu, self_sigma = self_["user_id"], self_["skill"], self_["uncertainty"]
+            opp_id, opp_mu, opp_sigma = opp_["user_id"], opp_["skill"], opp_["uncertainty"]
 
-        self_mu, self_sigma, opp_mu, opp_sigma = update_rating(
-            self_mu, self_sigma, self_score, opp_mu, opp_sigma, opp_score)
+            self_mu, self_sigma, opp_mu, opp_sigma = update_rating(
+                self_mu, self_sigma, self_score, opp_mu, opp_sigma, opp_score)
 
-        # Update database
-        db.execute(
-            """
-            UPDATE d_skill
-            SET skill = ?, uncertainty = ?
-            WHERE user_id = ?
-            """,
-            (self_mu, self_sigma, self_id),
-        )
-        db.commit()
+            # Update database
+            db.execute(
+                """
+                UPDATE d_skill
+                SET skill = ?, uncertainty = ?
+                WHERE user_id = ?
+                """,
+                (self_mu, self_sigma, self_id),
+            )
+            db.commit()
 
-        db.execute(
-            """
-            UPDATE d_skill
-            SET skill = ?, uncertainty = ?
-            WHERE user_id = ?
-            """,
-            (opp_mu, opp_sigma, opp_id),
-        )
-        db.commit()
+            db.execute(
+                """
+                UPDATE d_skill
+                SET skill = ?, uncertainty = ?
+                WHERE user_id = ?
+                """,
+                (opp_mu, opp_sigma, opp_id),
+            )
+            db.commit()
+            return redirect(url_for('index'))
+
+        flash(error)
 
     # Else if request method is GET
     return render_template('rank/update.html')
