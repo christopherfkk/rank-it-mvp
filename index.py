39c955fab13590c221fdc7dc5bca82796bedb510
christopherfkk <christopherfkk@gmail.com>
2023-02-26 22:43:29 +0530

    Different vercel setup

--- index.py
+++ index.py
@@ -1,1 +1,43 @@
-from application import app
+import os
+from flask import Flask
+
+from app.db import get_db
+
+db = get_db()
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)  # config files are relative to the instance folder located outside the "app" package
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config from config.py, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from app import db
+    db.init_app(app)
+
+    from app.auth import auth
+    app.register_blueprint(auth)  # auth.py: auth = BluePrint(...)
+
+    from app.rank import rank
+    app.register_blueprint(rank)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
+
+
+app = create_app()
