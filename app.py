6c3b7467ed79aef62739b4bb0d04197b0b868754
christopherfkk <christopherfkk@gmail.com>
2023-02-27 22:43:03 +0530

    basic

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from app import create_app
-from sqlalchemy import text
-from app.db import db
+# from app import create_app
+# from sqlalchemy import text
+# from app.db import db
+#
+#
+# app = create_app()
+# with app.app_context():
+#     with open('app/schema.sql', 'r') as f:
+#         schema = f.read()
+#         with db.engine.connect() as conn:
+#             conn.execute(text(schema))
+#             conn.commit()
+#
+# app.run()
 
+from flask import Flask
 
-app = create_app()
-with app.app_context():
-    with open('app/schema.sql', 'r') as f:
-        schema = f.read()
-        with db.engine.connect() as conn:
-            conn.execute(text(schema))
-            conn.commit()
+app = Flask(__name__)
 
-app.run()
+@app.route("/")
+def index():
+    return "This is yet another version!"
