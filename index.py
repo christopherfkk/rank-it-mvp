4b6eb73d8a18c2e79f8d5062175da411d617efcf
christopherfkk <christopherfkk@gmail.com>
2023-02-27 23:15:19 +0530

    change portsss

--- index.py
+++ index.py
@@ -4,13 +4,14 @@ from app.db import db
 import os
 
 
-app = create_app()
-with app.app_context():
-    with open('app/schema.sql', 'r') as f:
-        schema = f.read()
-        with db.engine.connect() as conn:
-            conn.execute(text(schema))
-            conn.commit()
+if __name__ == '__main__':
+    app = create_app()
+    with app.app_context():
+        with open('app/schema.sql', 'r') as f:
+            schema = f.read()
+            with db.engine.connect() as conn:
+                conn.execute(text(schema))
+                conn.commit()
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
