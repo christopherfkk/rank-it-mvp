0293b0078d09d185a08dc01163d4810ce97d7cb6
christopherfkk <christopherfkk@gmail.com>
2023-02-27 21:06:31 +0530

    Try waitress

--- index.py
+++ index.py
@@ -1,14 +1,16 @@
 from app import create_app
 from sqlalchemy import text
 from app.db import db
+from waitress import serve
 
-app = create_app()
 
-with app.app_context():
-    with open('app/schema.sql', 'r') as f:
-        schema = f.read()
-        with db.engine.connect() as conn:
-            conn.execute(text(schema))
-            conn.commit()
+if __name__ == "__main__":
+    app = create_app()
+    with app.app_context():
+        with open('app/schema.sql', 'r') as f:
+            schema = f.read()
+            with db.engine.connect() as conn:
+                conn.execute(text(schema))
+                conn.commit()
 
-app.run()
+    serve(app, host='0.0.0.0', port=5000)
